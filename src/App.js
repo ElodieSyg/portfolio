79b6b86e5eea3460b1a308eab4018687df3512fb
Elodie Sayavong <sayavong.pro@gmail.com>
2021-10-25 15:30:49 +0200

    App.js add router system

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 // Import views
 import Home from "./view/Home";
 import Project from "./view/Project";
@@ -5,8 +6,13 @@ import Contact from "./view/Contact";
 
 const App = () => {
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/project" component={Project} />
+        <Route exact path="/contact" component={Contact} />
+      </Switch>
+    </BrowserRouter>
   );
 };
 
