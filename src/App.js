eb6fc676511c93fa1c719e23cc14371f16c837dc
Elodie Sayavong <sayavong.pro@gmail.com>
2021-10-25 15:27:04 +0200

    Initialization project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// Import views
+import Home from "./view/Home";
+import Project from "./view/Project";
+import Contact from "./view/Contact";
+
+const App = () => {
+  return (
+    <>
+    </>
+  );
+};
+
+export default App;
